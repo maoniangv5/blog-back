ffa9ba88da21a654cf7dfb445a1cd635562c3f23
chem <846869169@qq.com>
2018-05-07 15:05:19 +0800

    fuck bug

--- service/tag/tagService.js
+++ service/tag/tagService.js
@@ -2,6 +2,6 @@
 let Tag = require('./model/tagBO');
 let ServiceGenerator = require('../../common/serviceGenerator');
 
-let TagService = ServiceGenerator.generate(BlTagog, '_id');
+let TagService = ServiceGenerator.generate(Tag, '_id');
 
 module.exports = TagService;
